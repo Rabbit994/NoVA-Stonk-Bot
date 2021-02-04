a16e6593cb938b5d51ef469b350f35888a322a39
Rabbit994 <12375329+Rabbit994@users.noreply.github.com>
2021-02-04 09:57:25 -0500

    adding error handling

--- app/main.py
+++ app/main.py
@@ -18,20 +18,24 @@ current_stock_index = 0
 
 async def change_presence():
     while True:
-        #print(f"Running loop")
-        global current_stock_index
-        global listofstonks
-        current_stock_symbol = listofstonks[current_stock_index]
-        current_stock_price = alpaca.get_stock_price(current_stock_symbol)
-        stock_str = f"{current_stock_symbol}: {current_stock_price}"
-        await client.change_presence(
-            activity=discord.Activity(type=discord.ActivityType.watching, name=stock_str)
-        )
-        if len(listofstonks) - 1 < current_stock_index + 1:
-            current_stock_index = 0
-        else:
-            current_stock_index += 1
-        await asyncio.sleep(30)
+        try:
+            #print(f"Running loop")
+            global current_stock_index
+            global listofstonks
+            current_stock_symbol = listofstonks[current_stock_index]
+            current_stock_price = alpaca.get_stock_price(current_stock_symbol)
+            stock_str = f"{current_stock_symbol}: {current_stock_price}"
+            await client.change_presence(
+                activity=discord.Activity(type=discord.ActivityType.watching, name=stock_str)
+            )
+            if len(listofstonks) - 1 < current_stock_index + 1:
+                current_stock_index = 0
+            else:
+                current_stock_index += 1
+            await asyncio.sleep(30)
+        except Exception as e:
+            print(e)
+            await asyncio.sleep(30)
 
 @client.event
 async def on_ready():
